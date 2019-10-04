63a5e199305204884f4f62d02749cd01fab8f11b
Ezio Shiki <zzyagg@gmail.com>
2019-10-04 14:58:13 +0800

    init

---
+++ src/n0001_two_sum.rs
@@ -0,0 +1,39 @@
+/**
+ * [1] Two Sum
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ *
+ */
+pub struct Solution {}
+
+// submission codes start here
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for (i, num_i) in nums.enumerate(){
+
+        }
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+    }
+}
